e589a492679004914b96e1b7d6f62e17765dfee8
NickHimmel <himmel.nick@gmail.com>
2020-09-17 15:31:38 -0400

    Add useContext to understand how it works, set up initial context to display name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './index'
 
 export default function App() {
+  const value = useContext(UserContext)
   return (
-    <div>App</div>
+    <div>Hello, {value}</div>
   );
 }
