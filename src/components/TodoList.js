3a2221353d852cb5d5295cbca53de9eefb9b51cf
NickHimmel <himmel.nick@gmail.com>
2020-09-24 14:35:13 -0400

    Add todo form and action to add todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,8 +13,8 @@ export default function TodoList() {
     }
 
     return (
-        <div className="container mx-auto max-w-md font-mono p-8 tracking-wide">
-            <h1 className="text-5xl border-b-4 border-double border-black">{title}</h1>
+        <div>
+            <h4 className="text-5xl border-b-4 border-double border-black">{title}</h4>
             <ul className="list-reset p-0 mt-10">
                 {state.todos.map((todo) => (
                     <li key={todo.id} className="">
@@ -23,13 +23,13 @@ export default function TodoList() {
                                 className="flex items-center flex-1 focus:outline-none"
                                 onClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}>
                                 <span className="material-icons mr-2">{ todo.complete ? "check_circle_outline" : "radio_button_unchecked" }</span>
-                                <span className={`${todo.complete && "line-through text-gray-900"}`}>{todo.text}</span>
+                                <span className={`${todo.complete && "line-through text-gray-900"} tracking-wide`}>{todo.text}</span>
                             </button>
                             <button className="flex items-center mr-5">
                                 <span className="material-icons text-gray-600">create</span>
                             </button>
                             <button 
-                                className="flex items-center"
+                                className="flex items-center focus:outline-none"
                                 onClick={() => handleDelete(todo)}>
                                 <span className="material-icons text-gray-600">delete</span>
                             </button>
