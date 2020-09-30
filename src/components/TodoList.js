47f7b2177fe3612dd33a65e92649368e55e494a2
NickHimmel <himmel.nick@gmail.com>
2020-09-30 17:29:57 -0400

    Move todo item to its own component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,13 @@
 import React, { useContext } from 'react'
+import Todo from './Todo'
 import TodosContext from '../context'
 
 export default function TodoList() {
-    const { state, dispatch } = useContext(TodosContext)
+    const { state } = useContext(TodosContext)
     const filteredTodos = state.todos.filter((todo) => !todo.complete)
     const title = filteredTodos.length > 0 
         ? `${filteredTodos.length} Todos` :
         "Nothing To Do!"
-
-    const handleDelete = (todo) => {
-        dispatch({ type: "DELETE_TODO", payload: todo })
-    }
-
-    console.log(state)
 
     return (
         <div>
@@ -20,25 +15,8 @@ export default function TodoList() {
             <ul className="list-reset p-0 mt-10">
                 {state.todos.map((todo) => (
                     <li key={todo.id}>
-                        <div className="flex text-xl my-5">
-                            <button 
-                                className="flex items-center flex-1 focus:outline-none"
-                                onClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}>
-                                <span className="material-icons mr-2">{ todo.complete ? "check_circle_outline" : "radio_button_unchecked" }</span>
-                                <span className={`${todo.complete && "line-through text-gray-900"} tracking-wide`}>{todo.text}</span>
-                            </button>
-                            <button 
-                                className="flex items-center mr-5"
-                                onClick={() => dispatch({ type: "SET_CURRENT_TODO", payload: todo })}>
-                                <span className="material-icons text-gray-600">create</span>
-                            </button>
-                            <button 
-                                className="flex items-center focus:outline-none"
-                                onClick={() => handleDelete(todo)}>
-                                <span className="material-icons text-gray-600">delete</span>
-                            </button>
-                        </div>
-                        <form className="flex my-5 hidden">
+                        <Todo todo={todo}/>
+                        <form className="flex my-5">
                             <input 
                                 className="border-b-2 border-double border-black focus:outline-none flex-1 mr-4"
                                 type="text"/>
